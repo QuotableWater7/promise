ee7569613b72ddd07d8432fb5c4cb1d075086d87
JT <jbowler2400@gmail.com>
2018-02-03 07:55:23 -0800

    First commit: working promise implementation with various tests

---
+++ src/index.js
@@ -0,0 +1,71 @@
+class CP {
+	constructor(cb) {
+		this.state = 'PENDING'
+		this.resolvedCallbacks = []
+		this.rejectedCallbacks = []
+
+		this.rejectionWasHandled = false
+
+		process.nextTick(() => {
+			try {
+				cb(
+					this.resolve.bind(this),
+					this.reject.bind(this)
+				)
+			} catch (e) {
+				this.reject(e)
+			}
+		})
+	}
+
+	resolve(value) {
+		this.state = 'RESOLVED'
+		this.value = value
+
+		this.resolvedCallbacks.forEach(({ success, error }) => {
+			try {
+				success(value)
+			} catch (e) {
+				error(e)
+			}
+		})
+	}
+
+	reject(error) {
+		this.state = 'REJECTED'
+		this.error = error
+
+		this.resolvedCallbacks.forEach(({ error: errorFn }) => {
+			errorFn(error)
+		})
+	}
+
+	then(successFn, errorFn) {
+		return new CP((resolve, reject) => {
+			if (this.state === 'RESOLVED') {
+				try {
+					resolve(successFn(this.value))
+				} catch (e) {
+					reject(e)
+				}
+			} else if (this.state === 'REJECTED' && errorFn) {
+				resolve(errorFn(this.error))
+			} else if (this.state === 'PENDING') {
+				this.resolvedCallbacks.push({
+					success: value => resolve(successFn(value)),
+					error: errorFn ? error => resolve(errorFn(error)) : () => null
+				})
+			}
+		})
+	}
+
+	catch(cb) {
+		if (this.state === 'REJECTED') {
+			cb(this.error)
+		} else if (this.state === 'PENDING') {
+			this.resolvedCallbacks.push({ error: cb })
+		}
+	}
+}
+
+module.exports = CP
---
+++ src/test/index.js
@@ -0,0 +1,89 @@
+const CP = require('..')
+
+function expectEqual(val1, val2) {
+	if (val1 !== val2) {
+		throw new Error(`Not equal: ${val1}, ${val2}`)
+	}
+}
+
+async function test(description, cb) {
+	try {
+		await cb()
+		console.log(`o ${description}`)
+	} catch (e) {
+		console.log(`x ${description}: ${e}`)
+	}
+}
+
+test('It works when value resolves immediately', async () => {
+	const result = await new CP(resolve => resolve(5))
+
+	expectEqual(result, 5)
+})
+
+test('It works when value resolves later', async () => {
+	const result = await new CP(resolve => {
+		setTimeout(() => resolve(5), 1)
+	})
+
+	expectEqual(result, 5)
+})
+
+test('It can catch thrown error in resolver', async () => {
+	const result = await new CP(resolve => {
+		throw new Error('nooo')
+	}).catch(error => {
+		expectEqual(error.message, 'nooo')
+	})
+})
+
+test('It can catch error immediately', async () => {
+	await new CP((resolve, reject) => {
+		reject('oops')
+	})
+		.catch(error => {
+			expectEqual(error, 'oops')
+		})
+})
+
+test('It can trap errors in try/catch', async () => {
+	try {
+		await new CP((resolve, reject) => {
+			reject('unhandled')
+		})
+	} catch (e) {
+		expectEqual(e, 'unhandled')
+	}
+})
+
+test('It can catch errors that happen in "then" chain', async () => {
+	await new CP((resolve, reject) => {
+		resolve(4)
+	})
+		.then(() => {
+			throw new Error('blah')
+		})
+		.catch(e => {
+			expectEqual(e.message, 'blah')
+		})
+})
+
+test('It can route errors to error handler in "then"', async () => {
+	await new CP((resolve, reject) => {
+		reject('shit')
+	})
+		.then(
+			() => null,
+			error => expectEqual(error, 'shit')
+		)
+})
+
+test('It can catch delayed errors', async () => {
+	await new CP((resolve, reject) => {
+		setTimeout(() => reject('boop'), 1)
+	})
+		.then(
+			() => null,
+			error => expectEqual(error, 'boop')
+		)
+})
